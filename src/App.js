10a92cae3350ec4c74987b89ee245710b66b4e15
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-24 16:03:30 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import './App.css';
+import Navbar from './components/layout/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+         <div>
+          <Navbar />
+         </div>
+      </BrowserRouter>
+      
+    );
+  }
+}
+
+export default App;

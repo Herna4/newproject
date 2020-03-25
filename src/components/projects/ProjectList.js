af39e9057cc7f75a3385772e0a76dc770b5d6564
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-24 20:57:11 -0400

    added some changes

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+
+           <div className="card z-depth-0 project-summary">
+             <div className="card-content grey-text text-darken-3">
+               <span className="card-title">Project Title</span>
+               <p>Posted by La Belle Herna</p>
+               <p className="grey-text">24th March, 19PM</p>
+            </div>
+           </div> 
+       
+           <div className="card z-depth-0 project-summary">
+             <div className="card-content grey-text text-darken-3">
+               <span className="card-title">Project Title</span>
+               <p>Posted by La Belle Herna</p>
+               <p className="grey-text">24th March, 19PM</p>
+            </div>
+           </div> 
+
+           <div className="card z-depth-0 project-summary">
+             <div className="card-content grey-text text-darken-3">
+               <span className="card-title">Project Title</span>
+               <p>Posted by La Belle Herna</p>
+               <p className="grey-text">24th March, 19PM</p>
+            </div>
+           </div> 
+
+    </div>
+
+        
+    )
+}
+
+export default ProjectList;

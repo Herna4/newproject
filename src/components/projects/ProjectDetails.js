49f6aa2bcfc5ae8f51be77798877e87f31d6c16d
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-24 22:32:45 -0400

    added some changes

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const ProjectDetails = (props) => {
-    console.log(props)
+    // console.log(props)
+    const id = props.match.params.id;
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                <div className="card-content">
-                   <span className="card-title">Project Title</span>
+                   <span className="card-title">Project Title - {id}</span>
                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                </div>
                <div className="card-action gret lighten-4 grey-text">

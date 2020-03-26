fffae1c45ef44f866e3d2806a3106d5059a371c7
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-26 15:07:21 -0400

    added data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div>
           <div className="card z-depth-0 project-summary">
              <div className="card-content grey-text text-darken-3">
-               <span className="card-title">Project Title</span>
+               <span className="card-title">{project.title}</span>
                <p>Posted by La Belle Herna</p>
                <p className="grey-text">24th March, 19PM</p>
             </div>
--- src/reducers/ProjectReducer.js
+++ src/reducers/ProjectReducer.js
@@ -1,7 +1,14 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'help for covid-19', content: 'It is very contagious'},
+        {id: '2', title: 'collect all the stars', content: 'blah blah blah'},
+        {id: '3', title: 'help for Haitian people', content: 'blah blah blah'}
+    ]
+}
 
 const ProjectReducer = (state = initState, action) => {
     return state
+
 }
 
 export default ProjectReducer

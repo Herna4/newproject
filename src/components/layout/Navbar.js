da1cc499714aa5b773e140c8cbee9c99a0138118
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-28 22:22:41 -0400

    added data

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import { connect } from 'react-redux'
 
  const Navbar = () => {
     return (
@@ -14,5 +15,11 @@ import SignedOutLinks from './SignedOutLinks'
         </nav>
     )
 }
+ 
+const mapStateToProps = (state) => {
+    console.log(state)
+    return{
 
-export default Navbar;
+    }
+} 
+export default connect(mapStateToProps)(Navbar);

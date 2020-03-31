1f0f69329df0db34e132cf4d950f99c479bf26e6
Herna Michel <labellehernamichel@yahoo.fr>
2020-03-31 15:25:34 -0400

    adding project

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -10,7 +10,7 @@ import { signOut } from '../../actions/authActions'
             <li><NavLink to='/createproject'>New Project</NavLink></li>
             <li><a onClick={props.signOut}>Log Out</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>
-             {/* {props.profile.initials} */}LBH
+             {props.profile.initials}
             </NavLink></li>
         </ul>
     )
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({project}) => {
           <div className="card z-depth-0 project-summary">
              <div className="card-content grey-text text-darken-3">
                <span className="card-title">{project.title}</span>
-               <p>Posted by La Belle Herna</p>
+    <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                <p className="grey-text">24th March, 19PM</p>
             </div>
            </div> 
